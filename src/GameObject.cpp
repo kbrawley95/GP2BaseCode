7cd7c60a5bc957247d7d2043467cbe24ba5764d0
kbrawley95 <kieran.brawley95@gmail.com>
2015-11-14 15:54:14 +0000

    GameObject Implementation (Class)
    The foundation of the GameObject-Component system

---
+++ src/GameObject.cpp
@@ -0,0 +1,141 @@
+#include "GameObject.h"
+#include "Shader.h"
+#include "Texture.h"
+
+GameObject::GameObject()
+{
+	m_VBO = 0;
+	m_EBO = 0;
+	m_NoOfIndices = 0;
+	m_NoOfVertices = 0;
+
+	m_ShaderProgram = 0;
+
+	m_ModelMatrix = mat4(1.0f);
+	m_Position = vec3(0.0f);
+	m_Rotation = vec3(0.0f);
+	m_Scale = vec3(1.0f);
+
+
+	m_AmbientMaterial = vec4(0.3f, 0.3f, 0.3f, 1.0f);
+	m_DiffuseMaterial = vec4(0.8f, 0.8f, 0.8f, 1.0f);
+	m_SpecularMaterial = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	m_SpecularPower = 25.0f;
+
+	m_ChildGameObjects.clear();
+
+	m_ParentGameObject = NULL;
+
+}
+
+GameObject::~GameObject()
+{
+	glDeleteBuffers(1, &m_VBO);
+	glDeleteBuffers(1, &m_EBO);
+	glDeleteVertexArrays(1, &m_VAO);
+	glDeleteProgram(m_ShaderProgram);
+	glDeleteTextures(1, &m_DiffuseMap);
+	m_ChildGameObjects.clear();
+}
+
+void GameObject::update()
+{
+	mat4 parentModel(1.0f);
+	if (m_ParentGameObject)
+	{
+		parentModel = m_ParentGameObject->getModelMatrix();
+	}
+
+	mat4 translationMatrix = translate(mat4(1.0f), m_Position);
+	mat4 scaleMatrix = scale(mat4(1.0f), m_Scale);
+
+	mat4 rotationMatrix = rotate(mat4(1.0f), m_Rotation.x, vec3(1.0f, 0.0f, 0.0f))*
+		rotate(mat4(1.0f), m_Rotation.y, vec3(0.0f, 1.0f, 0.0f))* rotate(mat4(1.0f), m_Rotation.z, vec3(0.0f, 0.0f, 1.0f));
+
+	m_ModelMatrix = scaleMatrix*rotationMatrix*translationMatrix;
+	m_ModelMatrix *= parentModel;
+
+	for (auto iter = m_ChildGameObjects.begin(); iter != m_ChildGameObjects.end(); iter++)
+	{
+		(*iter)->update();
+	}
+}
+
+void GameObject::addChild(shared_ptr<GameObject> child)
+{
+	child->m_ParentGameObject = this;
+	m_ChildGameObjects.push_back(child);
+}
+
+void GameObject::createBuffers(Vertex * pVerts, int numVerts, int *pIndices, int numIndices)
+{
+	m_NoOfIndices = numIndices;
+	m_NoOfVertices = numVerts;
+
+	//Generate Vertex Array
+	glGenVertexArrays(1, &m_VAO);
+	glBindVertexArray(m_VAO);
+
+	glGenBuffers(1, &m_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, numVerts*sizeof(Vertex), pVerts, GL_STATIC_DRAW);
+
+	//create buffer
+	glGenBuffers(1, &m_EBO);
+	//Make the EBO active
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+	//Copy Index data to EBO
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, numIndices*sizeof(int), pIndices,GL_STATIC_DRAW);
+	
+	//Tell the shader that 0 is the position element
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, colour));
+
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texCoords));
+
+	glEnableVertexAttribArray(3);
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+
+}
+
+void GameObject::loadShader(const string& vsFilename, const string& fsFilename)
+{
+	GLuint vertexShaderProgram = 0;
+	vertexShaderProgram = loadShaderFromFile(vsFilename, VERTEX_SHADER);
+	checkForCompilerErrors(vertexShaderProgram);
+
+	GLuint fragementShaderProgram = 0;
+	fragementShaderProgram = loadShaderFromFile(fsFilename, FRAGMENT_SHADER);
+	checkForCompilerErrors(fragementShaderProgram);
+
+	m_ShaderProgram = glCreateProgram();
+	glAttachShader(m_ShaderProgram, vertexShaderProgram);
+	glAttachShader(m_ShaderProgram, fragementShaderProgram);
+
+	//Link attributes
+	glBindAttribLocation(m_ShaderProgram, 0, "vertexPsosition");
+	glBindAttribLocation(m_ShaderProgram, 1, "vertexColour");
+	glBindAttribLocation(m_ShaderProgram, 2, "vertexTexCoords");
+	glBindAttribLocation(m_ShaderProgram, 3, "vertexNormal");
+
+	glLinkProgram(m_ShaderProgram);
+	checkForLinkErrors(m_ShaderProgram);
+	//now we cand elete the VS & FS Programs
+	glDeleteShader(vertexShaderProgram);
+	glDeleteShader(fragementShaderProgram);
+}
+
+void GameObject::loadDiffuseMap(const string& filename)
+{
+	m_DiffuseMap = loadTextureFromFile(filename);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glGenerateMipmap(GL_TEXTURE_2D);
+}
