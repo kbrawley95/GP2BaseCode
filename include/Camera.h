3618fbed4322f4f50e6341c1a4dbe8d2b37fa33e
kbrawley95 <kieran.brawley95@gmail.com>
2015-11-17 21:37:22 +0000

    Camera Class (Amendments)

--- include/Camera.h
+++ include/Camera.h
@@ -12,28 +12,12 @@ public:
 	mat4 viewMatrix;
 	mat4 projMatrix;
 
-	Camera(const vec3& pos, float fov, float aspect, float zNear, float zFar)
-	{
-		projMatrix = perspective(fov, aspect, zNear, zFar);
-		viewMatrix = lookAt(m_Position, m_Position + m_Forward, m_Up);
-		m_Position = pos;
-		m_Forward = vec3(0, 0, 1);
-		m_Up = vec3(0, 1, 0);
-	}
+	Camera(const vec3& pos, float fov, float aspect, float zNear, float zFar);
+	
 	~Camera();
 
-	void Update()
-	{
-		projMatrix;
-		viewMatrix = lookAt(m_Position, m_Position + m_Forward, m_Up);
-
-		mat4 translationMatrix = translate(mat4(1.0f), m_Position);
-		mat4 scaleMatrix = scale(mat4(1.0f), m_Scale);
-
-		mat4 rotationMatrix = rotate(mat4(1.0f), m_Rotation.x, vec3(1.0f, 0.0f, 0.0f))*
-			rotate(mat4(1.0f), m_Rotation.y, vec3(0.0f, 1.0f, 0.0f))*
-			rotate(mat4(1.0f), m_Rotation.z, vec3(0.0f, 0.0f, 1.0f));
-	}
+	void Update();
+	
 
 	mat4 GetLookAt();
 
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,5 +1,14 @@
 #include "Camera.h"
 
+Camera::Camera(const vec3& pos, float fov, float aspect, float zNear, float zFar)
+{
+	projMatrix = perspective(fov, aspect, zNear, zFar);
+	viewMatrix = lookAt(m_Position, m_Position + m_Forward, m_Up);
+	m_Position = pos;
+	m_Forward = vec3(0, 0, 1);
+	m_Up = vec3(0, 1, 0);
+}
+
 Camera::~Camera()
 {
 
@@ -11,5 +20,17 @@ mat4 Camera::GetLookAt()
 	return projMatrix * viewMatrix;
 }
 
+void Camera::Update()
+{
+	projMatrix;
+	viewMatrix = lookAt(m_Position, m_Position + m_Forward, m_Up);
+
+	mat4 translationMatrix = translate(mat4(1.0f), m_Position);
+	mat4 scaleMatrix = scale(mat4(1.0f), m_Scale);
+
+	mat4 rotationMatrix = rotate(mat4(1.0f), m_Rotation.x, vec3(1.0f, 0.0f, 0.0f))*
+		rotate(mat4(1.0f), m_Rotation.y, vec3(0.0f, 1.0f, 0.0f))*
+		rotate(mat4(1.0f), m_Rotation.z, vec3(0.0f, 0.0f, 1.0f));
+}
 
 
